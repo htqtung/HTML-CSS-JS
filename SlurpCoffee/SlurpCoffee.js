06840e39344380edf82c467586e3e6436de54172
htqtung <31605439+htqtung@users.noreply.github.com>
2018-03-02 14:16:30 +0200

    Slurp Test

--- SlurpCoffee/SlurpCoffee.js
+++ SlurpCoffee/SlurpCoffee.js
@@ -2,9 +2,9 @@
 /*global console*/
 /*global window*/
 
-const SMALL_BAG_VOLUME = 16*23*2;
-const MEDIUM_BAG_VOLUME = 22*26*2;
-const LARGE_BAG_VOLUME = 14*26*10;
+const SMALL_BAG_VOLUME = 16 * 23 * 2;
+const MEDIUM_BAG_VOLUME = 22 * 26 * 2;
+const LARGE_BAG_VOLUME = 14 * 26 * 10;
 
 
 function run() {
@@ -21,23 +21,25 @@ function run() {
         boxSize = Number(txtBoxSize),
         numOfBox,
         divAnswer;
-    
+
     // Log values
-    console.log(smallBag + ' ' + mediumBag + ' ' + largeBag + ' ' + boxSize );
+    console.log(smallBag + ' ' + mediumBag + ' ' + largeBag + ' ' + boxSize);
 
     // error checking
-    if(smallBag < 0 || mediumBag < 0 || largeBag < 0 || boxSize < 30 || boxSize > 100) {
+    if (smallBag < 0 || mediumBag < 0 || largeBag < 0 || boxSize < 30 || boxSize > 100) {
         console.log("Invalid Input");
-        divAnswer = "Please enter valid number, for coffee bags, they should not be smaller than 0 </br> and valid box sizes are from 30cm to 100cm.";
+        alert("Please enter valid number, for coffee bags, they should not be smaller than 0 and valid box sizes are from 30cm to 100cm.");
+        divAnswer = "";
     }
-    else if(smallBag == 0 && mediumBag == 0 && largeBag == 0) {
+    else if (smallBag == 0 && mediumBag == 0 && largeBag == 0) {
         console.log("The order is invalid. All three coffee bag fields are entered with 0.");
-        divAnswer = "All three fields of coffee bag cannot be 0.";
+        alert("All three fields of coffee bag cannot be 0.");
+        divAnswer = "";
     }
     // calculate number of boxes needed
     else {
         numOfBox = calculateNumberOfBox(smallBag, mediumBag, largeBag, boxSize);
-        if(numOfBox <= 1) {
+        if (numOfBox <= 1) {
             divAnswer = "You need " + numOfBox + " box of size " + boxSize + "cm to fit all the bags.";
         }
         else {
@@ -45,7 +47,7 @@ function run() {
         }
     }
 
-    
+
     // set answer to the div Answer
     document.getElementById("divAnswer").innerHTML = divAnswer;
     // clear input boxes
@@ -59,8 +61,8 @@ function run() {
 //The function will return float number and I use Math.ceil() to return the number of box instead of round() because we need room also for the odd part.
 function calculateNumberOfBox(smallBag, mediumBag, largeBag, boxSize) {
     var numOfBox, temp;
-    const BOX_VOLUME = boxSize*boxSize*boxSize;
-    temp = (smallBag*SMALL_BAG_VOLUME + mediumBag*MEDIUM_BAG_VOLUME + largeBag*LARGE_BAG_VOLUME)/BOX_VOLUME;
+    const BOX_VOLUME = boxSize * boxSize * boxSize;
+    temp = (smallBag * SMALL_BAG_VOLUME + mediumBag * MEDIUM_BAG_VOLUME + largeBag * LARGE_BAG_VOLUME) / BOX_VOLUME;
     console.log("Calculation result: " + ((smallBag * SMALL_BAG_VOLUME + mediumBag * MEDIUM_BAG_VOLUME + largeBag * LARGE_BAG_VOLUME) / BOX_VOLUME).valueOf());
     numOfBox = Math.ceil(temp);
     return numOfBox;
